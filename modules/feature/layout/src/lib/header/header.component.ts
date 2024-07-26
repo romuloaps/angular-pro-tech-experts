e70366514f21d73e3685481a1b42ddee86711278
Romulo Augusto <romulomail@gmail.com>
2024-07-25 22:25:35 -0300

    refactor(layout): modifica o prefixo do componente header para 'layout' no lugar de 'lib' e atualiza o eslint do módulo para validar o novo prefixo

--- modules/feature/layout/src/lib/header/header.component.ts
+++ modules/feature/layout/src/lib/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'lib-header',
+  selector: 'layout-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {}
